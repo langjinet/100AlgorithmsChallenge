e40b822da61b452fda51840e4244e8d2c6617020
Hao Diep <haodiep@localhost.localdomain>
2019-06-09 11:49:35 +0700

    finished execises

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,11 @@
-function addBorder(picture: string[]): string[] {
-
+function addBorder(pictures: string[]): string[] {
+    var wall = '*'.repeat(pictures[0].length + 2)
+    pictures = pictures.map(function (s) {
+        return '*'.concat(s, '*');
+    })
+    pictures.unshift(wall);
+    pictures.push(wall);
+    return pictures;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,10 @@
 function adjacentElementsProduct(inputArray: number[]): number {
-
+    let largest = inputArray[0] * inputArray[1];
+    for(let i = 1; i < inputArray.length - 1; i++ ) {
+        let product = inputArray[i] * inputArray[i+1];
+        largest = largest < product ? product : largest;
+    }
+    return largest;
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
